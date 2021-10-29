d8a54744346b7207276d6ecf54367771b309ac3d
Robert Kurczak <robertkurczak.studia@gmail.com>
2021-10-29 18:17:33 +0200

    gravitation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 #include "random"
+#include "iostream"
 
 class Cell{
     public:
-        int x, y, lifeTime, value;
+        float x, y;
+        int lifeTime, value;
 
-        Cell(int x, int y, int lifeTime = -1, int value = 1){
+        Cell(float x, float y, int lifeTime = -1, int value = 1){
             this->x = x;
             this->y = y;
             this->lifeTime = lifeTime;
@@ -17,9 +19,17 @@ class Cell{
 class LiquidSimulator : public olc::PixelGameEngine{
     private:
         std::vector<std::vector<char>> matrix;
+        std::vector<Cell> waterCells;
+
+        double simulationTime = 0;
+        std::unique_ptr<olc::Sprite> spriteSheet;
+        olc::vi2d tileSize = {4, 4};
 
     public:
         bool OnUserCreate() override{
+            //Load sprite sheet
+            spriteSheet = std::make_unique<olc::Sprite>("./Sprites/tiles.png");
+
             //---Initialization of cellular automaton matrix---
             for(int y = 0; y < ScreenHeight(); y++){
                 std::vector<char> row;
@@ -33,26 +43,69 @@ class LiquidSimulator : public olc::PixelGameEngine{
             //------
 
             //---For tests---
-            for(int y = 0; y < ScreenHeight() / 2; y++){
-                matrix[y][y % 2] = 1;
-            }
+            // matrix[0][0] = 1;
+            // matrix[0][1] = 2;
+            // matrix[0][2] = 3;
+            // matrix[0][3] = 4;
+
+            waterCells.push_back(Cell(100, 20));
+            matrix[20][100] = 2;
 
             return true;
         }
 
         bool OnUserUpdate(float fElapsedTime) override{
-            //Clear(olc::BLACK);
+            simulationTime += fElapsedTime;
 
+            Clear(olc::BLACK);
+
+            //---Rendering matrix---
             for(int y = 0; y < ScreenHeight(); y++){
                 for(int x = 0; x < ScreenWidth(); x++){
-                    if(matrix[y][x]) Draw(x, y, olc::WHITE);
+                    switch(matrix[y][x]){
+                        case 1:
+                            DrawPartialSprite(olc::vi2d(x, y) * tileSize, spriteSheet.get(), olc::vi2d(0, 0) * tileSize, tileSize);
+
+                            break;
+
+                        case 2:
+                            DrawPartialSprite(olc::vi2d(x, y) * tileSize, spriteSheet.get(), olc::vi2d(1, 0) * tileSize, tileSize);
+
+                            break;
+
+                        case 3:
+                            DrawPartialSprite(olc::vi2d(x, y) * tileSize, spriteSheet.get(), olc::vi2d(2, 0) * tileSize, tileSize);
+
+                            break;
+
+                        case 4:
+                            DrawPartialSprite(olc::vi2d(x, y) * tileSize, spriteSheet.get(), olc::vi2d(3, 0) * tileSize, tileSize);
+
+                            break;
+                        
+                        case 5:
+                            DrawPartialSprite(olc::vi2d(x, y) * tileSize, spriteSheet.get(), olc::vi2d(4, 0) * tileSize, tileSize);
+
+                            break;
+                    }
+                }
+            }
+            //------
+
+            for(int i = 0; i < waterCells.size(); i++){
+                float newPositionY = (9.81 * simulationTime * simulationTime) / 2.f;
+
+                if(newPositionY < 90){
+                    matrix[waterCells[i].y][waterCells[i].x] = 0;
+                    waterCells[i].y = newPositionY;
+                    matrix[newPositionY][waterCells[i].x] = 2;
+
+                    std::cout << newPositionY << std::endl;
                 }
             }
 
-            // waterCell.y = startPositionY + (9.81 * simulationTime * simulationTime) / 2.f;
-
             return true;
-	}
+        }
 
 };
 std::vector<Cell> activeCells;
@@ -62,7 +115,7 @@ int main(){
     //---Creating window and start simulation---
     LiquidSimulator LS;
 
-    if(LS.Construct(256, 256, 1, 1)){
+    if(LS.Construct(640, 360, 2, 2)){
 		LS.Start();
     }
     //------
