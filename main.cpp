924586457c34c3b4b4c0ba93ca31d2c3ed66cf26
Robert Kurczak <robertkurczak.studia@gmail.com>
2021-11-07 22:06:08 +0100

    new model

--- main.cpp
+++ main.cpp
@@ -1,33 +1,45 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
-#include "random"
+#include "cstdlib"
 #include "iostream"
 
-class Cell{
-    public:
-        float x, y, velocity;
-        int lifeTime, value;
+#define solidBlockID 999
 
-        Cell(float x, float y, int value = 1, int lifeTime = -1, float velocity = 1.f)
-        : x(x), y(y), value(value), lifeTime(lifeTime), velocity(velocity){}
-};
+// class Cell{
+
+//     public:
+//         float waterValue, velocity;
+//         Cell(float waterValue, float velocity) : waterValue(waterValue), velocity(velocity){}
+// };
 
 class LiquidSimulator : public olc::PixelGameEngine{
     private:
         std::unique_ptr<olc::Sprite> spriteSheet;
         olc::vi2d tileSize = {4, 4};
 
-        std::vector<std::vector<char>> matrix;
+        char maxWaterValue = 4;
+        float compression = 0.1;
+
+        //If divider would have value of 1, no states inbetween
+        //of water flowing from cell to cell would be rendered
+        float flowDivider = 2;
+
+        //We have to stop dividing at some point
+        float minFlow = 0.5;
+
+        std::vector<std::vector<float>> matrix;
+
+        //Need to be initialized in OnUserCreate()
         olc::vi2d matrixSize;
-        std::vector<Cell> waterCells;
 
-        float waterAcceleration = 5.f;
+        double clock = 0;
+        double updateInterval = 0.02;
 
-        char getNeighbour(Cell targetCell, olc::vi2d versor){
-            int positionX = targetCell.x + versor.x;
-            int positionY = targetCell.y + versor.y;
+        int getNeighbour(olc::vi2d currentPosition, olc::vi2d versor){
+            int positionX = currentPosition.x + versor.x;
+            int positionY = currentPosition.y + versor.y;
 
-            if(positionX <= matrixSize.x - 1 && positionY <= matrixSize.y - 1){
+            if(positionX < matrixSize.x && positionY < matrixSize.y){
                 return matrix[positionY][positionX];
             }
             else{
@@ -35,17 +47,23 @@ class LiquidSimulator : public olc::PixelGameEngine{
             }
         }
 
-        void updateCellPosition(Cell &targetCell, olc::vf2d position){
-            if(position.x > matrixSize.x - 1 || position.y > matrixSize.y - 1) return;
-            
-            matrix[targetCell.y][targetCell.x] = 0;
+        float waterFlowDown(float source, float sink){
+            float sum = source + sink;
 
-            targetCell.x = position.x;
-            targetCell.y = position.y;
-
-            matrix[targetCell.y][targetCell.x] = targetCell.value;
+            //If all water from source will fit in the sink
+            if(sum <= maxWaterValue){
+                return source;
+            }
+            //If not all water from source will fit in the sink and source wouldn't be full
+            //It means that bottom cell will become compressed but only proportionally to the amount of water above
+            else if(sum < (2 * maxWaterValue + compression)){
+                return (maxWaterValue * maxWaterValue + sum * compression) / (maxWaterValue + compression) - sink;
+            }
+            else{
+                return ((sum + compression) / 2) - sink;
+            }
         }
-    
+
     public:
         bool OnUserCreate() override{
             matrixSize = {ScreenWidth() / tileSize.x, ScreenHeight() / tileSize.y};
@@ -55,46 +73,69 @@ class LiquidSimulator : public olc::PixelGameEngine{
 
             //---Initialization of cellular automaton matrix---
             for(int y = 0; y < matrixSize.y; y++){
-                std::vector<char> row;
+                std::vector<float> row;
 
                 for(int x = 0; x < matrixSize.x; x++){
-                    //Filling everything with 0, except borders, which are 5
-                    row.push_back((y == 0 || x == 0 || y == matrixSize.y - 1 || x == matrixSize.x - 1) * 5);
+                    //Filling everything with 0, except borders
+                    row.push_back((y == 0 || x == 0 || y == matrixSize.y - 1 || x == matrixSize.x - 1) * solidBlockID);
                 }
 
                 matrix.push_back(row);
             }
             //------
 
-            //---Creating test objects---
-            waterCells.push_back(Cell(5, 5, 4));
-            //------
-
-            //---Adding waterCells to matrix---
-            for(int i = 0; i < waterCells.size(); i++){
-                Cell currentCell = waterCells[i];
-
-                matrix[currentCell.y][currentCell.x] = currentCell.value;
-            }
-            //------
-
-            matrix[8][4] = 5;
+            //matrix[8][4] = 5;
+            matrix[3][5] = 4;
 
             return true;
         }
 
         bool OnUserUpdate(float fElapsedTime) override{
+            //---Spawn water cell on Enter---
             if(GetKey(olc::Key::ENTER).bPressed){
-                waterCells.push_back(Cell(1, 5, 4));
-                matrix[5][1] = 4;
+                matrix[3][5] = 4;
+            }//------
+
+            //---Executing simulation every update interval---
+            clock += fElapsedTime;
+
+            if(clock < updateInterval){
+                return true;
             }
+            else{
+                clock -= updateInterval;
+            }
+            //------
 
             Clear(olc::BLACK);
+
+            //---Simulation step---
+            for(int y = 0; y < matrixSize.y; y++){
+                for(int x = 0; x < matrixSize.x; x++){
+
+                    float currentCell = matrix[y][x];
+                    float bottomtCell = getNeighbour(olc::vi2d(x, y), olc::vi2d(0, 1));
+
+                    if(currentCell == -1 || currentCell == solidBlockID) continue;
+                    if(bottomtCell == -1 || bottomtCell == solidBlockID) continue;
+
+                    float waterToFlow = waterFlowDown(currentCell, bottomtCell) / flowDivider;
+
+                    if(waterToFlow > minFlow) waterToFlow /= flowDivider;
+
+                    matrix[y][x] -= waterToFlow;
+                    matrix[y + 1][x] += waterToFlow;
+                }
+            }
+            //------
 
             //---Rendering matrix---
             for(int y = 0; y < matrixSize.y; y++){
                 for(int x = 0; x < matrixSize.x; x++){
-                    switch(matrix[y][x]){
+                    switch(int(matrix[y][x])){
+                        case 0:
+                            break;
+
                         case 1:
                             DrawPartialSprite(olc::vi2d(x, y) * tileSize, spriteSheet.get(), olc::vi2d(0, 0) * tileSize, tileSize);
 
@@ -110,78 +151,19 @@ class LiquidSimulator : public olc::PixelGameEngine{
 
                             break;
 
-                        case 4:
+                        default:
                             DrawPartialSprite(olc::vi2d(x, y) * tileSize, spriteSheet.get(), olc::vi2d(3, 0) * tileSize, tileSize);
 
                             break;
                         
-                        case 5:
+                        //Rework this to enum or something
+                        case solidBlockID:
                             DrawPartialSprite(olc::vi2d(x, y) * tileSize, spriteSheet.get(), olc::vi2d(4, 0) * tileSize, tileSize);
 
                             break;
                     }
                 }
             }
-            //------
-
-            //Bufor for storing new water cells, created in simulation step
-            std::vector<Cell> newWaterCells;
-
-            //---Simulation step---
-            for(int i = 0; i < waterCells.size(); i++){
-                Cell &currentCell = waterCells[i];
-                char bottomNeighbour = getNeighbour(currentCell, olc::vi2d(0, 1));
-
-                //Falling down case
-                if(bottomNeighbour != -1 && bottomNeighbour < currentCell.value){
-                    char waterToFlow = currentCell.value - bottomNeighbour;
-                    
-                    updateCellPosition(currentCell, olc::vd2d(currentCell.x, currentCell.y + currentCell.velocity * fElapsedTime));
-                    currentCell.velocity += waterAcceleration * fElapsedTime;
-                    currentCell.value += bottomNeighbour;
-                }
-                else if(bottomNeighbour == 5 && GetKey(olc::Key::SPACE).bPressed){
-                    //Spilling on left site and then right site
-                    for(char j = -1; j <= 1; j+=2){
-                        //Making sure cell object have same value as in matrix
-                        currentCell.value = matrix[currentCell.y][currentCell.x];
-
-                        char neighbour = getNeighbour(currentCell, olc::vi2d(j, 0));
-
-                        //If neighbour isn't wall or isn't outside of screen
-                        if(neighbour != 5 && neighbour != -1){
-                            
-                            if(neighbour < currentCell.value && currentCell.value > 1){
-                                char waterToSpill = currentCell.value / 2;
-                                //If can spill in other direction
-                                if(getNeighbour(currentCell, olc::vi2d(j * -1, 0)) < currentCell.value){
-                                    waterToSpill = currentCell.value / 3;
-                                }
-
-                                //That means that waterCell objest doesn't exist yet
-                                if(neighbour == 0){
-                                    newWaterCells.push_back(Cell(currentCell.x + j, currentCell.y, 1));
-                                }
-
-                                matrix[currentCell.y][currentCell.x + j] += waterToSpill;
-                                matrix[currentCell.y][currentCell.x] -= waterToSpill;
-
-                                //I directly modified matrix without looking for proper waterCell object (for performance),
-                                //so now, values in matrix don't match with values stored in waterCell objects.
-                                //That's why at beggining of this for loop I synchornise those.
-                            }
-                        }
-                    }
-                }
-            }
-
-            //Adding newly created water cells to main vector
-            for(int i = 0; i < newWaterCells.size(); i++){
-                waterCells.push_back(newWaterCells[i]);
-            }
-
-            newWaterCells = {};
-
             //------
 
             return true;
@@ -194,7 +176,7 @@ int main(){
     //---Creating window and start simulation---
     LiquidSimulator LS;
 
-    if(LS.Construct(40, 40, 20, 20)){
+    if(LS.Construct(100, 100, 5, 5)){
 		LS.Start();
     }
     //------
